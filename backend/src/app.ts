3334cf8b98e00416f7aab1210ae9f202ae104bf4
Thy Tran <22521461@gm.uit.edu.vn>
2025-11-10 22:39:52 +0700

    fix: update swagger.json file serving path for consistency

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import compression from "compression";
 import swaggerUi from "swagger-ui-express";
 import { RegisterRoutes } from "./generated/routes";
+import path from "path";
 
 const app = express();
 
@@ -42,7 +43,7 @@ app.use(
 
 // Serve swagger.json
 app.get("/swagger.json", (req, res) => {
-  res.sendFile(__dirname + "/generated/swagger.json");
+  res.sendFile(path.join(__dirname, "generated", "swagger.json"));
 });
 
 // TSOA generated routes (auto-generated from controllers)
